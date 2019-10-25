1a8ac2550e345e02914b192ba75f6d00ecd35e28
z.w.cool <zwcool@gmx.net>
2019-10-25 23:09:58 +0200

    Implemented a solution with future and promise but it won't work. The section with condition variable are commented below

--- consoleToFile/app/main.cpp
+++ consoleToFile/app/main.cpp
@@ -1,3 +1,74 @@
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <queue>
+#include <future>
+
+void writeToFile(std::string input)
+{
+    std::ofstream file("console-output.txt", std::ofstream::out | std::ofstream::app);
+    if (file.is_open())
+    {
+        file << input;
+    }
+    else
+    {
+        std::cout << "Don't forget an else path if you open a file!" << std::endl;
+    }
+}
+
+/* std::future with std::promise are supposed to not work within a loop
+   See for more details:
+   https://stackoverflow.com/questions/45649987/stdfuture-error-when-using-stdpromise
+*/
+int main ()
+{
+    std::queue<std::string> produced_input;
+    bool is_done = false;
+    bool is_notified = false;
+    bool console_has_close = false;
+    std::promise<bool> prm;
+    std::future<bool> func = prm.get_future();
+    std::thread producer ([&]() {
+        while(!console_has_close)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::cout << "Write something..." << std::endl;
+            std::string input;
+            std::getline(std::cin, input);
+            input += "\n";                  // Each new line after nen returned input
+            produced_input.push(input);
+            prm.set_value(true);
+        }
+    });
+
+    std::thread consumer([&]() {
+        while(!console_has_close)
+        {
+            func.wait();
+            if(func.get())
+            {
+                while (!produced_input.empty())
+                {
+                    writeToFile(produced_input.front());
+                    produced_input.pop();
+                }
+            }
+        }
+    });
+
+    producer.join();
+    consumer.join();
+}
+
+
+
+/*  ###################################################
+    ############## condition_variables ################
+    ###################################################
+
+
 #include <condition_variable>
 #include <fstream>
 #include <iostream>
@@ -63,3 +134,6 @@ int main ()
     producer.join();
     consumer.join();
 }
+
+*/
+
